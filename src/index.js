0cf7b85cf26f43cfb4f1c314a1a23baad90fc0d7
Masrur00 <91047001+Masrur00@users.noreply.github.com>
2022-03-31 15:50:36 +0530

    Only 6 lines of controllers and their drivers add

--- src/index.js
+++ src/index.js
@@ -12,6 +12,17 @@ app.use(cors());
 const userController = require("./controllers/user.controller")
 const {register,login} = require("../src/controllers/signup.constrollers")
 
+
+// Controllers for products of mens, womens, jwelleries
+const mensController = require('./controllers/mens.controllers');
+const womensController = require('./controllers/womens.controllers');
+const jwelleryController = require('./controllers/jwelleries.controllers');
+
+app.use("/products", mensController);
+app.use("/womens", womensController);
+app.use("/jwelleries", jwelleryController);
+// Ended only 6 lines added 
+
 app.use("/users", userController)
 
 app.post("/register", register)
