84de161bd077885847b2e104c53fc526cfafffef
Tanvi Samant <96070177+tanviSAM@users.noreply.github.com>
2022-03-31 13:30:04 +0530

    Add files via upload

---
+++ script/womens.js
@@ -0,0 +1,414 @@
+var dataarr = [
+    {
+        image: "https://images.meesho.com/images/products/38038484/xzpnb_512.jpg",
+        title: 'Trendy Elegant Women Dresses',
+        mrp: 399,
+        price: 299,
+        rating: 4.0,
+        review: 9
+    },
+    {
+        image: "https://images.meesho.com/images/products/20543549/666a5_512.jpg",
+        title: 'Trendy Feminine Women Dresses',
+        mrp: 692,
+        price: 592,
+        rating: 4.5,
+        review: 1
+    },
+    {
+        image: "https://images.meesho.com/images/products/68485169/kha44_512.jpg",
+        title: 'Aagyeyi Ensemble Sarees',
+        mrp: 767,
+        price: 667,
+        rating: 5.0,
+        review: 3
+    },
+    {
+        image: "https://images.meesho.com/images/products/22043185/5e8c8_512.jpg",
+        title: 'Fancy Glamorous Women Jumpsuits',
+        mrp: 482,
+        price: 282,
+        rating: 5.0,
+        review: 1
+    },
+    {
+        image: "https://images.meesho.com/images/products/41486612/uwtcu_512.jpg",
+        title: 'Classy Retro Women Dresses',
+        mrp: 312,
+        price: 219,
+        rating: 3.9,
+        review: 11
+    },
+    {
+        image: "https://images.meesho.com/images/products/41671721/hesuu_512.jpg",
+        title: 'Stylish Feminine Women Dresses',
+        mrp: 384,
+        price: 284,
+        rating: 4.0,
+        review: 7
+    },
+    {
+        image: "https://images.meesho.com/images/products/41692850/ggttb_512.jpg",
+        title: 'Trendy Fashionista Women Tops & Tunics',
+        mrp: 539,
+        price: 439,
+        rating: 3.1,
+        review: 1
+    },
+    {
+        image: "https://images.meesho.com/images/products/42200696/wlzso_512.jpg",
+        title: 'Urbane Glamorous Women dress',
+        mrp: 318,
+        price: 223,
+        rating: 3.8,
+        review: 3
+    },
+    {
+        image: "https://images.meesho.com/images/products/31875333/jod7j_512.jpg",
+        title: 'Urbane Fashionable Women Tops & Tunics',
+        mrp: 279,
+        price: 196,
+        rating: 3.7,
+        review: 16
+    },
+    {
+        image: "https://images.meesho.com/images/products/74867438/g96re_512.jpg",
+        title: 'Pretty Ravishing Women Jumpsuits',
+        mrp: 1762,
+        price: 1662,
+        rating: 4.4,
+        review: 16
+    },
+    {
+        image: "https://images.meesho.com/images/products/75791003/uhfln_512.jpg",
+        title: 'Urbane Feminine Women Tshirts ',
+        mrp: 388,
+        price: 288,
+        rating: 5.0,
+        review: 24
+    },
+    {
+        image: "https://images.meesho.com/images/products/68479303/vgqoe_512.jpg",
+        title: 'Classy Graceful Women Tshirts ',
+        mrp: 259,
+        price: 182,
+        rating: 4.7,
+        review: 5
+    },
+    {
+        image: "https://images.meesho.com/images/products/71563717/9tecn_512.jpg",
+        title: 'Trendy Retro Women Tshirts ',
+        mrp: 201,
+        price: 197,
+        rating: 5.0,
+        review: 22
+    },
+    {
+        image: "https://images.meesho.com/images/products/19212145/fe39a_512.jpg",
+        title: 'Urbane Fashionable Women Jumpsuits',
+        mrp: 558,
+        price: 458,
+        rating: 5.0,
+        review: 2
+    },
+    {
+        image: "https://images.meesho.com/images/products/41713399/k4dve_512.jpg",
+        title: 'Fancy Glamorous Women Tops & Tunics',
+        mrp: 448,
+        price: 348,
+        rating: 4.3,
+        review: 15
+    },
+    {
+        image: "https://images.meesho.com/images/products/40122510/w7ubm_512.jpg",
+        title: 'Classic Modern Women Dresses',
+        mrp: 348,
+        price: 248,
+        rating: 4.0,
+        review: 8
+    },
+    {
+        image: "https://images.meesho.com/images/products/40191150/4b0la_512.jpg",
+        title: 'Pretty Latest Women Dresses',
+        mrp: 505,
+        price: 405,
+        rating: 3.9,
+        review: 2
+    },
+    {
+        image: "https://images.meesho.com/images/products/69421093/xh4vf_512.jpg",
+        title: 'Pretty Fabulous Women Sweatshirts',
+        mrp: 1063,
+        price: 945,
+        rating: 4.0,
+        review: 9
+    },
+    {
+        image: "https://images.meesho.com/images/products/10556602/e81f7_512.jpg",
+        title: 'Classic Retro Women Jackets & Waistcoat',
+        mrp: 1489,
+        price: 1389,
+        rating: 3.7,
+        review: 4
+    },
+    {
+        image: "https://images.meesho.com/images/products/40589552/incom_512.jpg",
+        title: 'Trendy Partywear Women Dresses',
+        mrp: 482,
+        price: 382,
+        rating: 3.0,
+        review: 4
+    },
+    {
+        image: "https://images.meesho.com/images/products/71087503/5b1tu_512.jpg",
+        title: 'Comfy Feminine Women Tshirts ',
+        mrp: 262,
+        price: 162,
+        rating: 4.0,
+        review: 30
+    },
+    {
+        image: "https://images.meesho.com/images/products/45666424/ygpx3_512.jpg",
+        title: 'Stylish Fashionista Women Jumpsuits',
+        mrp: 462,
+        price: 362,
+        rating: 4.3,
+        review: 7
+    },
+    {
+        image: "https://images.meesho.com/images/products/61696039/vdky4_512.jpg",
+        title: 'Pretty Graceful Women Tshirts ',
+        mrp: 559,
+        price: 459,
+        rating: 5.0,
+        review: 9
+    },
+    {
+        image: "https://images.meesho.com/images/products/42368448/gmomk_512.jpg",
+        title: 'Classic Fabulous Women Dresses',
+        mrp: 660,
+        price: 560,
+        rating: 4.2,
+        review: 5
+    },
+    {
+        image: "https://images.meesho.com/images/products/42601250/u7nft_512.jpg",
+        title: 'Comfy Fashionable Women Dress',
+        mrp: 515,
+        price: 415,
+        rating: 2.8,
+        review: 0
+    },
+    {
+        image: "https://images.meesho.com/images/products/42623042/jwa0l_512.jpg",
+        title: 'Classy Sensational Women Tops & Tunics',
+        mrp: 304,
+        price: 215,
+        rating: 3.4,
+        review: 21
+    },
+    {
+        image: "https://images.meesho.com/images/products/40773957/axkh6_512.jpg",
+        title: 'Comfy Designer Women Tops & Tunics',
+        mrp: 299,
+        price: 210,
+        rating: 4.1,
+        review: 2
+    },
+    {
+        image: "https://images.meesho.com/images/products/58547810/tlrsd_512.jpg",
+        title: 'Fashionable Modern Women Western Skirts',
+        mrp: 398,
+        price: 358,
+        rating: 4.5,
+        review: 9
+    },
+    {
+        image: "https://images.meesho.com/images/products/71563721/bdjfc_512.jpg",
+        title: 'Classy Fashionable Women Tshirts ',
+        mrp: 281,
+        price: 197,
+        rating: 4.8,
+        review: 18
+    },
+    {
+        image: "https://images.meesho.com/images/products/22923958/41cc4_512.jpg",
+        title: 'Comfy Fashionista Women Jeans',
+        mrp: 531,
+        price: 431,
+        rating: 4.3,
+        review: 6
+    },
+    {
+        image: "https://images.meesho.com/images/products/64339049/53ybn_512.jpg",
+        title: 'Pretty Latest Women Tshirts ',
+        mrp: 671,
+        price: 571,
+        rating: 3.8,
+        review: 16
+    },
+    {
+        image: "https://images.meesho.com/images/products/42388018/jtfdh_512.jpg",
+        title: 'Trendy Graceful Women Tops & Tunics',
+        mrp: 444,
+        price: 344,
+        rating: 3.6,
+        review: 11
+    },
+    {
+        image: "https://images.meesho.com/images/products/55768312/5bi7n_512.jpg",
+        title: 'Stylish Glamorous Women Sweatshirts',
+        mrp: 500,
+        price: 400,
+        rating: 3.8,
+        review: 4
+    },
+    {
+        image: "https://images.meesho.com/images/products/77834860/vxz65_512.jpg",
+        title: 'Comfy Partywear Women Top & Bottom Sets',
+        mrp: 482,
+        price: 382,
+        rating: 2.0,
+        review: 1
+    },
+    {
+        image: "https://images.meesho.com/images/products/18664836/8bfc7_512.jpg",
+        title: 'Fancy Retro Women Dresses',
+        mrp: 525,
+        price: 425,
+        rating: 5.0,
+        review: 15
+    },
+    {
+        image: "https://images.meesho.com/images/products/71532442/yanck_512.jpg",
+        title: 'Fancy Feminine Women Dresses',
+        mrp: 446,
+        price: 346,
+        rating: 4.8,
+        review: 3
+    }
+
+];
+
+appenddata(dataarr)
+
+function appenddata(data) {
+    data.forEach((el) => {
+
+        var product_div = document.createElement("div")
+        product_div.setAttribute("id", "product_div")
+        product_div.addEventListener("click", function () {
+            product_description(el);
+        })
+
+
+        var product_img = document.createElement("img");
+        product_img.src = el.image;
+
+        var product_title = document.createElement("p")
+        product_title.setAttribute("id", "product_title")
+        product_title.innerText = el.title;
+
+        //creating a div to store product price-------------------------------------------
+
+        var product_price_div = document.createElement("div")
+        product_price_div.setAttribute("id", "product_price_div")
+
+        var product_price = document.createElement("p");
+        product_price.setAttribute("id", "product_price")
+        product_price.innerText = `₹${el.price}`;
+
+        var mrp = document.createElement("p")
+        mrp.setAttribute("id", "mrp");
+        mrp.innerText = `₹${el.mrp}`;
+
+        var per_discount = document.createElement("p")
+        per_discount.setAttribute("id", "per_discount")
+        per_discount.innerText = `${Math.round(Math.random() * (50 - 20) + 20)}% off`;
+
+        product_price_div.append(product_price, mrp, per_discount)
+
+        //creating a div to store extra discount------------------------------------------------------------------------
+
+        var ext_discount = document.createElement("div")
+        ext_discount.setAttribute("id", "ext_discount")
+
+        var discount_img = document.createElement("img")
+        discount_img.setAttribute("id", "discount_img")
+        discount_img.src = "https://i.postimg.cc/Kzk9hV5x/discount-3.png"
+
+        var text = document.createElement("p")
+        text.setAttribute("id", "text")
+        text.innerText = `₹100 discount on 1st order`
+
+        ext_discount.append(discount_img, text)
+
+
+        //creating a div to store the free delivery--------------------------------------------------------------------
+
+        var delivery = document.createElement("div")
+        delivery.setAttribute("id", "delivery")
+
+        var product_delivery = document.createElement("p")
+        product_delivery.setAttribute("id", "product_delivery")
+        product_delivery.innerText = "Free Delivery"
+
+        delivery.append(product_delivery);
+
+        //creating a div to store the product rating and product review-----------------------------------------------------
+
+        var product_rating_div = document.createElement("div");
+        product_rating_div.setAttribute("id", "product_rating_div")
+
+
+        product_rating_container = document.createElement("div")
+        product_rating_container.setAttribute("id", "product_rating_container")
+
+
+        var product_rating = document.createElement("p")
+        product_rating.setAttribute("id", "product_rating")
+        product_rating.innerText = `${el.rating}⋆`;
+
+        product_rating_container.append(product_rating);
+
+        var product_review = document.createElement("p");
+        product_review.setAttribute("id", "product_review")
+        product_review.innerText = `${el.review} Reviews`
+
+        product_rating_div.append(product_rating_container, product_review)
+
+        product_div.append(product_img, product_title, product_price_div, ext_discount, delivery, product_rating_div)
+
+        document.querySelector("#container").append(product_div)
+    });
+}
+
+
+function handleprice() {
+
+    var shortv = document.querySelector("#select_tag").value
+
+    if (shortv == "high") {
+        dataarr.sort(function (a, b) {
+            return Number(b.price) - Number(a.price);
+        });
+    }
+    if (shortv == "low") {
+        dataarr.sort(function (a, b) {
+            return Number(a.price) - Number(b.price);
+        });
+    }
+    if (shortv == "rating") {
+        dataarr.sort(function (a, b) {
+            return Number(b.rating) - Number(a.rating)
+        })
+    }
+    document.querySelector("#container").innerHTML = "";
+    appenddata(dataarr);
+}
+
+function product_description(el) {
+    data = JSON.stringify(el)
+    localStorage.setItem("product_detail", data)
+    window.location.href = 'product_description.html'
+}
